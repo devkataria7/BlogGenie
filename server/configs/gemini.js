962dd9a03b716df5c53cb43e4a0f7df45e52c8cd
devkataria7 <devkataria2093@gmail.com>
2026-07-28 20:36:02 +0530

    Update Gemini model and improve Git configuration

--- server/configs/gemini.js
+++ server/configs/gemini.js
@@ -5,7 +5,7 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 async function main(prompt) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
+    model: "gemini-flash-latest",
     contents: prompt,
   });
   return response.text;
